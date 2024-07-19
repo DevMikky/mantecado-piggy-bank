573872607d2770db12617d8c12cb584ec7ef3810
Mikky <progmdpl96@hotmail.com>
2024-07-18 23:01:14 -0600

    Primer commit de mantecado piggy bank

---
+++ src/main/java/com/mikky/app/rest/MantecadoPiggyBankApplication.java
@@ -0,0 +1,13 @@
+package com.mikky.app.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantecadoPiggyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantecadoPiggyBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikky/app/rest/MantecadoPiggyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikky.app.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantecadoPiggyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
